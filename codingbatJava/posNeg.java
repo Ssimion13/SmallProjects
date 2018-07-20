8eb8b89c83b6ceb1b883759a3a6d829992309423
ssimion13 <scottsimion@gmail.com>
2018-07-19 23:03:34 -0700

    will finish first ten questions today

---
+++ codingbatJava/posNeg.java
@@ -0,0 +1,13 @@
+//posNeg
+
+
+/* Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, 
+then return true only if both are negative. */
+
+public boolean posNeg(int a, int b, boolean negative){
+    if( ( ( (a > 0 && b < 0) || (a < 0 && b > 0) ) && !negative ) || (negative && (a < 0 && b < 0 ) ) ){
+        return true;
+    } else {
+        return false;
+    }
+}
