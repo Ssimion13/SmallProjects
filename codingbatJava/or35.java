8c271084f3fc80af4c64d9872641d29a62d2aa25
ssimion13 <scottsimion@gmail.com>
2018-07-20 19:13:24 -0700

    5 more warmups for today

---
+++ codingbatJava/or35.java
@@ -0,0 +1,14 @@
+//or35
+
+
+/* 
+Return true if the given non-negative number is a multiple of 3 or a multiple of 5. 
+*/
+
+
+public boolean or35 (int n) {
+    if ( (n % 3 == 0) || (n % 5 == 0)){
+        return true;
+    }
+    return false;
+}
